bc71b1f286e1c6e943a06686cbe1574bed849968
lsl <2460497138@qq.com>
2023-08-26 19:28:54 +0800

    header样式以及退出按钮

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,30 @@
 
 <script>
-import {Setting,Fold} from '@element-plus/icons-vue'
+import { Setting, Fold, Bell, ArrowDown } from '@element-plus/icons-vue'
 export default {
   name: 'home',
-  components:{
-    setting:Setting,
-    fold:Fold
+  components: {
+    setting: Setting,
+    fold: Fold,
+    bell: Bell,
+    ArrowDown
+  },
+  data() {
+    return {
+      userInfo: {
+        userName: 'lsl',
+        userEmail: 'lsl@email.com'
+      }
+    }
+  },
+  methods: {
+    handleLogout(key) {
+      console.log(key);
+      if(key == "email") return
+      this.$store.commit('saveUserInfo','')
+      this.userInfo = null
+      this.$router.push('/login')
+    }
   }
 }
 </script>
@@ -20,29 +39,22 @@ export default {
         <span>Manager</span>
       </div>
       <!-- 菜单部分 collapse控制菜单折叠-->
-      <el-menu
-        default-active="2"
-        class="nav-menu"
-        background-color="#001529"
-        text-color="#fff"
-        :collapse="false"
-        router
-      >
+      <el-menu default-active="2" class="nav-menu" background-color="#001529" text-color="#fff" :collapse="false" router>
         <el-sub-menu index="1">
           <template #title>
             <setting class="menu-setting"></setting>
             <span>系统管理</span>
           </template>
-            <el-menu-item index="1-1">用户管理</el-menu-item>
-            <el-menu-item index="1-2">菜单管理</el-menu-item>
+          <el-menu-item index="1-1">用户管理</el-menu-item>
+          <el-menu-item index="1-2">菜单管理</el-menu-item>
         </el-sub-menu>
         <el-sub-menu index="2">
           <template #title>
             <setting class="menu-setting"></setting>
             <span>审批管理</span>
           </template>
-            <el-menu-item index="2-1">休假管理</el-menu-item>
-            <el-menu-item index="2-2">待我审批</el-menu-item>
+          <el-menu-item index="2-1">休假管理</el-menu-item>
+          <el-menu-item index="2-2">待我审批</el-menu-item>
         </el-sub-menu>
       </el-menu>
     </div>
@@ -56,7 +68,27 @@ export default {
           <div class="bread">面包屑</div>
         </div>
         <!-- 右侧用户信息 -->
-        <div class="user-info">用户信息</div>
+        <div class="user-info">
+          <el-badge :is-dot="true" class="user-badge">
+            <el-icon class="el-icon-bell">
+              <bell></bell>
+            </el-icon>
+          </el-badge>
+          <el-dropdown class="user-dropdown" @command="handleLogout">
+            <span class="user-link">
+              {{ userInfo.userName }}
+              <el-icon class="el-icon--right">
+                <arrow-down />
+              </el-icon>
+            </span>
+            <template #dropdown>
+              <el-dropdown-menu>
+                <el-dropdown-item command="email">邮箱：{{ userInfo.userEmail }}</el-dropdown-item>
+                <el-dropdown-item command="logout">退出</el-dropdown-item>
+              </el-dropdown-menu>
+            </template>
+          </el-dropdown>
+        </div>
       </div>
       <!-- 下面区域 -->
       <div class="wrapper">
@@ -82,27 +114,36 @@ export default {
     color: #fff;
     overflow-y: auto; //滚动条
     transition: width 0.5s;
-    .logo{
+
+    .logo {
       display: flex;
       align-items: center;
       font-size: 18px;
       height: 50px;
-      img{
+
+      img {
         margin: 0 15px;
         width: 32px;
         height: 32px;
       }
-  
+
+      span {}
+
+
     }
-    .nav-menu{
+
+    .nav-menu {
       border-right: none;
       height: calc(100vh - 50px);
-      .menu-setting{
+
+      .menu-setting {
         width: 20px;
         height: 20px;
         margin-right: 12px;
       }
- 
+
+      span {}
+
     }
   }
 
@@ -116,17 +157,41 @@ export default {
       justify-content: space-between;
       border-bottom: 1px solid #ddd;
       padding: 0 20px;
+
       .nav-left {
         display: flex;
         align-items: center;
+
         .menu-fold {
           width: 25px;
           height: 25px;
           margin-right: 15px;
         }
-        
+
+        .bread {}
+
       }
-     
+
+      .user-info {
+        .user-badge {
+          line-height: 30px;
+          margin-right: 15px;
+
+          .el-icon-bell {}
+        }
+
+        .user-dropdown {
+          margin-top: 15px;
+
+          .user-link {
+            cursor: pointer;
+            color: #409eff;
+            .el-icon--right {}
+          }
+        }
+
+      }
+
 
     }
 
