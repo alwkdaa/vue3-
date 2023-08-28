c54be2f7c4e9405aa46458d1631b7436d9d2b5d7
lsl <2460497138@qq.com>
2023-08-28 23:28:30 +0800

    菜单管理的新增模态框

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -26,7 +26,7 @@
           :width="item.width" :formatter="item.formatter" />
         <el-table-column label="操作" width="220">
           <template #default="scope">
-            <el-button type="primary" size="mini" @click="handleAdd(2,scope.row)">新增</el-button>
+            <el-button type="primary" size="mini" @click="handleAdd(2, scope.row)">新增</el-button>
             <el-button type="primary" size="mini" @click="handleEdit(scope.row)">编辑</el-button>
             <el-button type="danger" size="mini" @click="handleDelete(scope.row)">删除</el-button>
           </template>
@@ -37,134 +37,173 @@
     <el-dialog title="菜单新增" v-model="showModal" :before-close="handleCloseDialog">
       <el-form :model="menuForm" ref="dialogForm" label-width="100px" :rules="rules">
         <el-form-item prop="parentId" label="父级菜单">
-          <el-cascader v-model="menuForm.parentId" :options="menuList" :props="{ checkStrictly:true,value:'_id', label:'menuName' }" clearable></el-cascader>
+          <el-cascader v-model="menuForm.parentId" :options="menuList"
+            :props="{ checkStrictly: true, value: '_id', label: 'menuName' }" clearable></el-cascader>
           <span>不选择默认是创建一级菜单</span>
         </el-form-item>
         <el-form-item prop="menuType" label="菜单类型">
           <el-radio-group v-model="menuForm.menuType">
-            <el-radio :value="1">菜单</el-radio>
-            <el-radio :value="2">按钮</el-radio>
+            <el-radio :label="1">菜单</el-radio>
+            <el-radio :label="2">按钮</el-radio>
           </el-radio-group>
         </el-form-item>
         <el-form-item prop="menuName" label="菜单名称">
           <el-input placeholder="请输入菜单名称" v-model="menuForm.menuName"></el-input>
         </el-form-item>
-        <el-form-item prop="icon" label="菜单图标">
+        <el-form-item prop="icon" label="菜单图标" v-show="menuForm.menuType === 1">
           <el-input placeholder="请输入菜单图标" v-model="menuForm.icon"></el-input>
         </el-form-item>
-        <el-form-item prop="path" label="路由地址">
+        <el-form-item prop="path" label="路由地址" v-show="menuForm.menuType === 1">
           <el-input placeholder="请输入路由地址" v-model="menuForm.path"></el-input>
         </el-form-item>
-        <el-form-item prop="menuCode" label="权限标识">
+        <el-form-item prop="menuCode" label="权限标识" v-show="menuForm.menuType === 2">
           <el-input placeholder="请输入权限标识" v-model="menuForm.menuCode"></el-input>
         </el-form-item>
-        <el-form-item prop="component" label="组件路径">
+        <el-form-item prop="component" label="组件路径" v-show="menuForm.menuType === 1">
           <el-input placeholder="请输入组件路径" v-model="menuForm.component"></el-input>
         </el-form-item>
-        <el-form-item prop="menuState" label="菜单状态">
+        <el-form-item prop="menuState" label="菜单状态" v-show="menuForm.menuType === 1">
           <el-radio-group v-model="menuForm.menuState">
-            <el-radio :value="1">正常</el-radio>
-            <el-radio :value="2">停用</el-radio>
+            <el-radio :label="1">正常</el-radio>
+            <el-radio :label="2">停用</el-radio>
           </el-radio-group>
         </el-form-item>
       </el-form>
       <template #footer>
-      <span class="dialog-footer">
-        <el-button @click="handleClose">取消</el-button>
-        <el-button type="primary" @click="handleSubmit">
-          确定
-        </el-button>
-      </span>
-    </template>
+        <span class="dialog-footer">
+          <el-button @click="handleClose">取消</el-button>
+          <el-button type="primary" @click="handleSubmit">
+            确定
+          </el-button>
+        </span>
+      </template>
     </el-dialog>
   </div>
 </template>
 <script>
 import utils from '../utils/utils';
-export default{
-  name:'Menu',
-  data(){
-    return{
+export default {
+  name: 'Menu',
+  data() {
+    return {
       // 菜单表单
-      queryForm:{
-        menuState:1,
+      queryForm: {
+        menuState: 1,
       },
-      menuForm:{},
-      menuList:[],
+      menuForm: {
+        menuType: 1,
+        menuState: 1
+      },
+      menuList: [],
       // 控制新增编辑的模态框显示
-      showModal:false,
+      showModal: false,
       // 新增和编辑的标识
       action: 'add',
       // table列的配置
-      columns:[
+      columns: [
         {
-          label:'菜单名称',
-          prop:'menuName',
+          label: '菜单名称',
+          prop: 'menuName',
         },
         {
-          label:'图标',
-          prop:'icon',
+          label: '图标',
+          prop: 'icon',
         },
         {
-          label:'权限标识',
-          prop:'menuCode',
+          label: '权限标识',
+          prop: 'menuCode',
         },
         {
-          label:'路由地址',
-          prop:'path',
+          label: '路由地址',
+          prop: 'path',
         },
         {
-          label:'组件路径',
-          prop:'component',
+          label: '组件路径',
+          prop: 'component',
         },
         {
-          label:'菜单状态',
-          prop:'menuState',
-          formatter(row,column,value){
-            return{
-              1:'正常',
-              2:'停用'
+          label: '菜单状态',
+          prop: 'menuState',
+          formatter(row, column, value) {
+            return {
+              1: '正常',
+              2: '停用'
             }[value]
           }
         },
         {
-          label:'创建时间',
-          prop:'createTime',
-          formatter(row,column,value){
+          label: '创建时间',
+          prop: 'createTime',
+          formatter(row, column, value) {
             return utils.formateDate(new Date(value))
           }
         },
-      ]
+      ],
+      // 表单验证规则
+      rules: {
+        menuName: [
+          {
+            required: true,
+            message: '请输入菜单的名称',
+            trgger:"blur"
+          },
+          {
+            min:2,
+            max:10,
+            message: '长度在2-8个字符',
+            trgger:"blur"
+          }
+        ]
+      }
     }
   },
-  mounted(){
+  mounted() {
     this.getMenuList()
   },
-  methods:{
+  methods: {
     // 调用获取菜单数据接口
-    async getMenuList(params){
+    async getMenuList(params) {
       let res = await this.$api.menuList(params)
       console.log(res);
       this.menuList = res
     },
     // 查询按钮
-    handleQuery(){
+    handleQuery() {
       console.log("111");
     },
     // 重置按钮
-    handleReset(){
-      console.log("111");
+    handleReset(form) {
+      this.$refs[form].resetFields()
     },
     // 创建按钮
-    handleAdd(){
-
+    handleAdd(type, row) {
+      this.showModal = true
+      this.action = "add"
+      if(type == 2){
+        // console.log("2");
+        this.menuForm.parentId = [...row.parentId, row._id].filter(item => item)
+      }
     },
     // 编辑按钮
-    handleEdit(){
+    handleEdit() {
 
     },
     // 删除按钮
-    handleDelete(){
+    handleDelete() {
+
+    },
+    // 模态框的叉号
+    handleCloseDialog() {
+      this.handleReset('dialogForm')
+      this.showModal = false
+    },
+    // 模态框取消按钮
+    handleClose() {
+      this.handleReset('dialogForm')
+      this.showModal = false
+    },
+    // 模态框确认按钮
+    handleSubmit() {
 
     }
   }
